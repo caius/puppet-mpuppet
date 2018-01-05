acc01d75467b251e151c606be24f083203ffced3
Caius Durling <dev@caius.name>
2018-01-05 00:07:44 +0000

    Expect the branch file to always exist

--- files/mpuppet-fetch.sh
+++ files/mpuppet-fetch.sh
@@ -15,14 +15,13 @@ exec 1> >(logger -s -t "$(basename "$0")") 2>&1
 
 readonly PUPPET_DIR="/etc/puppetlabs/code/environments/production"
 readonly PUPPET="/opt/puppetlabs/bin/puppet"
+readonly BRANCH_FILE="/etc/puppetlabs/puppet/release_branch"
 
-readonly BRANCH_OVERRIDE_FILE="/etc/puppetlabs/puppet/branch"
-if [[ -f $BRANCH_OVERRIDE_FILE ]]; then
-  readonly PUPPET_BRANCH=$(cat $BRANCH_OVERRIDE_FILE)
-else
-  readonly PUPPET_BRANCH="origin/master"
+if [[ ! -f $BRANCH_FILE ]]; then
+  echo "Error: release_branch file missing" >> /dev/stderr
+  exit 1
 fi
 
 export GIT_DIR="${PUPPET_DIR}"
 git fetch origin
-git checkout -fq "${PUPPET_BRANCH}"
+git checkout -fq "$(cat $BRANCH_FILE)"
