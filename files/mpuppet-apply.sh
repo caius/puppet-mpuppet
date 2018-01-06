4b3672d02a5ec5e44937a5195695a68897111abf
Caius Durling <dev@caius.name>
2018-01-06 16:12:40 +0000

    Use correct paths based on filesystem layout

--- files/mpuppet-apply.sh
+++ files/mpuppet-apply.sh
@@ -17,18 +17,19 @@ if [[ -x /opt/puppetlabs/bin/puppet ]]; then
   # Debian-esque installed puppet from puppetlabs deb release
   readonly PUPPET="/opt/puppetlabs/bin/puppet"
   readonly LIBRARIAN_PUPPET="/opt/puppetlabs/puppet/bin/librarian-puppet"
+  readonly PUPPET_DIR="/etc/puppetlabs/code/environments/production"
   export PATH=/opt/puppetlabs/bin:$PATH
 elif [[ -x /opt/local/bin/puppet ]]; then
   # SmartOS-esque installed puppet from gem under /opt/local
   readonly PUPPET="/opt/local/bin/puppet"
   readonly LIBRARIAN_PUPPET="/opt/local/bin/librarian-puppet"
+  readonly PUPPET_DIR="/opt/local/etc/puppetlabs/code/environments/production"
 else
   # Something installed puppet from gem under /usr/local
   readonly PUPPET="/usr/local/bin/puppet"
   readonly LIBRARIAN_PUPPET="/usr/local/puppet/bin/librarian-puppet"
+  readonly PUPPET_DIR="/etc/puppetlabs/code/environments/production"
 fi
-
-readonly PUPPET_DIR="/etc/puppetlabs/code/environments/production"
 
 (cd ${PUPPET_DIR}; $LIBRARIAN_PUPPET install)
 
--- files/mpuppet-fetch.sh
+++ files/mpuppet-fetch.sh
@@ -13,9 +13,22 @@ set -o noclobber
 
 exec 1> >(tee >(logger -t $(basename $0))) 2>&1
 
-readonly PUPPET_DIR="/etc/puppetlabs/code/environments/production"
-readonly PUPPET="/opt/puppetlabs/bin/puppet"
-readonly BRANCH_FILE="/etc/puppetlabs/puppet/release_branch"
+if [[ -x /opt/puppetlabs/bin/puppet ]]; then
+  # Debian-esque installed puppet from puppetlabs deb release
+  readonly PUPPET="/opt/puppetlabs/bin/puppet"
+  readonly PUPPET_DIR="/etc/puppetlabs/code/environments/production"
+  readonly BRANCH_FILE="/etc/puppetlabs/puppet/release_branch"
+elif [[ -x /opt/local/bin/puppet ]]; then
+  # SmartOS-esque installed puppet from gem under /opt/local
+  readonly PUPPET="/opt/local/bin/puppet"
+  readonly PUPPET_DIR="/opt/local/etc/puppetlabs/code/environments/production"
+  readonly BRANCH_FILE="/opt/local/etc/puppetlabs/puppet/release_branch"
+else
+  # Something installed puppet from gem under /usr/local
+  readonly PUPPET="/usr/local/bin/puppet"
+  readonly PUPPET_DIR="/etc/puppetlabs/code/environments/production"
+  readonly BRANCH_FILE="/etc/puppetlabs/puppet/release_branch"
+fi
 
 if [[ ! -f $BRANCH_FILE ]]; then
   echo "Error: release_branch file missing" >> /dev/stderr
